b61dda3c39f684b12bb42ae8ee2b177cbe9bb1c8
Greg Hale <imalsogreg@gmail.com>
2014-10-31 14:09:57 -0400

    Drop false-positives due to isInfixOf Refactor to make search terms and searched-channels more explicit Allow some terms to be prefix-matches, others full-word matches Update dependencies and haskell language version, to Haskell2010

--- src/EtaBot/Main.hs
+++ src/EtaBot/Main.hs
@@ -2,27 +2,35 @@
 
 module Main where
 
-import           IO
 import           Control.Monad
 import qualified Data.ByteString as B
 import           Data.Maybe
 import qualified Data.Text as T
 import           Data.Text.Encoding
 import           Network.SimpleIRC
+import           System.IO
 
 botName :: String
 botName = "etabot"
 
+watchChannels :: [T.Text]
+watchChannels = ["#snapframework", "#haskell", "#nothaskell"]
+
+noticeWords :: [T.Text]
+noticeWords = ["snap","heist","xmlhtml","tango"]
+
+noticePrefixes :: [T.Text]
+noticePrefixes = ["snaplet-"]
+
 config :: IrcConfig
-config = defaultConfig
-    { cAddr = "irc.freenode.net"
-    , cPort = 6667
-    , cNick = botName
-    , cUsername = botName
-    , cRealname = botName
-    , cChannels = ["#snapframework", "#haskell"]
-    , cEvents = [notifySnap]
-    }
+config =
+  let cfg = mkDefaultConfig "irc.freenode.net" botName
+  in cfg { cPort = 6667
+         , cUsername = botName
+         , cRealname = botName
+         , cChannels = ["#snapframework", "#haskell"]
+         , cEvents = [notifySnap]
+         }
 
 onDecodeError descr input = Nothing
 
@@ -39,26 +47,38 @@ foo = T.concat . T.splitOn "snapshot"
 notifySnap :: IrcEvent
 notifySnap = Privmsg $ \mirc imsg -> do
     let actualMsg = decodeUtf8With onDecodeError $ mMsg imsg
-        msg = T.concat $ T.splitOn "snapshot" $ T.toLower $ decodeUtf8With onDecodeError $ mMsg imsg
         speaker = fromMaybe "Someone" (mNick imsg)
         chan = fromMaybe "<unknown>" (mChan imsg)
-        checkNotify str = when (str `T.isInfixOf` msg) $ do
-            let out = B.concat
-                    [ speaker 
-                    , " is talking about " 
-                    , encodeUtf8 str 
-                    , " in " 
-                    , chan 
-                    , ": " 
-                    , encodeUtf8 actualMsg
-                    ]
-            sendMsg mirc "#snapframework" out
-            putStrLn $ T.unpack $ decodeUtf8 out
-    if chan == "#haskell"
-      then do checkNotify "snap"
-              checkNotify "heist"
-              checkNotify "xmlhtml"
-      else return ()
+        res  = eavesdropResponse (decodeUtf8 speaker) (decodeUtf8 chan)
+               actualMsg
+    maybe (return ()) (sendMsg mirc "#snapframework" . encodeUtf8) res
+
+dropToWord :: T.Text -> T.Text
+dropToWord = T.filter (\c -> c `elem` ['a'..'z'] || c `elem` ['A'..'Z'])
+
+dropEndPunctuation :: T.Text -> T.Text
+dropEndPunctuation = T.reverse . T.dropWhile (`elem` ".,!") . T.reverse
+
+eavesdropResponse :: T.Text -> T.Text -> T.Text -> Maybe T.Text
+eavesdropResponse speaker chan msg
+  | chan `elem` watchChannels =
+    let rWith x = T.concat [ speaker , " is talking about " , x
+                           , " in ", chan, ": ", msg]
+    in case msgKeywords msg of
+      []  -> Nothing
+      [k] -> Just $ rWith k
+      ks  -> Just . rWith $
+             T.concat [ T.intercalate ", " (init ks), " and ", last ks]
+  | otherwise = Nothing
+
+msgKeywords :: T.Text -> [T.Text]
+msgKeywords msg =
+  [k | k <- noticeWords
+     , T.toUpper k `elem` map T.toUpper (map dropToWord $ T.words msg)]
+  ++
+  [dropEndPunctuation w
+  | w <- T.words msg
+  , any (\k -> T.toUpper k `T.isPrefixOf` T.toUpper w) noticePrefixes]
 
 main :: IO ()
 main = do
